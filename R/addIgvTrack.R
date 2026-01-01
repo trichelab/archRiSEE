de4b64d3fc40d8ada1bbcfcc04714889651c1f4b
Tim Triche, Jr. <tim.triche@gmail.com>
2026-01-01 18:18:13 -0500

    do not setGenome(igv, unique(genome(x))) to avoid clobbering tracks

--- R/addIgvTrack.R
+++ R/addIgvTrack.R
@@ -2,21 +2,23 @@
 #' 
 #' @param   x   SingleCellExperiment with weights in mcols(x)$trk
 #' @param   trk name of the column in mcols(x) with weights to plot
-#' @param   igv an igvR object (genome will be set to match unique(genome(x)))
+#' @param   igv an igvR object with an appropriate genome to match genome(x)
 #' @param   co  color for the track (violet)
 #' @param   a   autoscale the track? (TRUE)
 #' @param   ... additional parameters passed to igvR::GRangesQuantitativeTrack
 #' 
-#' @return  none, called for side effects 
+#' @return  nothing useful (called for side effects)
 #' 
 #' @examples
 #' library(igvR)
 #' igv <- igvR()
+#' setGenome(igv, "hg38")
 #' mcols(SCE)$aTrack <- rpois(nrow(SCE), lambda=50)
 #' addIgvTrack(SCE, "aTrack", igv) 
 #'
 #' @seealso igvR::GRangesQuantitativeTrack
 #' @seealso igvR::displayTrack
+#' @seealso igvR::setGenome
 #' @seealso igvR::igvR
 #'
 #' @import  igvR
@@ -26,15 +28,26 @@
 addIgvTrack <- function(x, trk, igv, co="violet", a=TRUE, ...) {
 
   stopifnot(is(igv, "igvR"))
+  g <- unique(genome(x))
+  if (is.na(g)) {
+    warning("No genome currently set for x; fix this to avoid major mishaps.")
+  } else { 
+    stopifnot(g %in% getSupportedGenomes(igv))
+    message("Please be sure to setGenome(igv, ", g, ") before adding tracks.")
+  }
+  rr <- .makeIgvTrackFromRowRanges(x, trk)  
+  displayTrack(igv, 
+               GRangesQuantitativeTrack(trk, rr, color=co, autoscale=a, ...))
+
+}
+
+
+# helper fn
+.makeIgvTrackFromRowRanges <- function(x, trk) { 
+
   rr <- rowRanges(x)
-  setGenome(igv, unique(genome(rr)))
   mcols(rr) <- mcols(rr)[, trk, drop=FALSE]
   names(mcols(rr)) <- "score"
-  invisible(displayTrack(igv, 
-                         GRangesQuantitativeTrack(trk,
-                                                  rr, 
-                                                  color=co, 
-                                                  autoscale=a, 
-                                                  ...)))
+  return(rr)
 
 }
